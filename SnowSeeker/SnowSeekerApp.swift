390895b1a945a62955fdfb131aa91ee0340bae3f
netolobo <desilio@gmail.com>
2023-12-13 13:53:37 -0800

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Neto Lobo on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
